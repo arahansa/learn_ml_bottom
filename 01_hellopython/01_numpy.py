1b57e89cc0012cf6aaf27278ec34e6891ce6fc0b
arahansa <arahansa@naver.com>
2017-01-18 00:23:05 +0900

    hello python

---
+++ 01_hellopython/01_numpy.py
@@ -0,0 +1,25 @@
+
+
+import numpy as np
+
+x = np.array([1.0, 2.0, 3.0])
+
+print(x)
+
+x = np.array([1.0, 2.0, 3.0])
+y = np.array([2.0, 4.0, 6.0])
+
+
+print("x", x)
+print("y", y)
+
+print("더하기", x+y)
+
+print("곱하기", x * y)
+
+
+
+
+
+
+
