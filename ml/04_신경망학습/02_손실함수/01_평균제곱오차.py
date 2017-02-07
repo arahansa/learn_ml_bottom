83b34d97e01f3b7b7bce15d2212997e652c428b0
arahansa <arahansa@naver.com>
2017-02-07 23:50:33 +0900

    add cross entropy_err

--- ml/04_신경망학습/02_손실함수/01_평균제곱오차.py
+++ ml/04_신경망학습/02_손실함수/01_평균제곱오차.py
@@ -12,3 +12,16 @@ print(mean_squared_error(np.array(y),np.array(t)))
 # 예 7일 확률이 높다고 추정했을 때
 y = [ 0.1, 0.05, 0.1, 0.0, 0.05, 0.1, 0.0, 0.6, 0.0, 0.0]
 print(mean_squared_error(np.array(y),np.array(t)))
+
+
+
+def cross_entropy_error(y,t):
+    delta = 1e-7
+    return -np.sum(t * np.log(y + delta))
+
+t = [0,0,1,0,0,0,0,0,0,0]
+y = [0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0]
+
+print("---")
+print(cross_entropy_error(np.array(y), np.array(t)))
+
