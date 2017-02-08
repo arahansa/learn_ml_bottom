697cce3dcb32e08a7a98633b6f76903e2aef08ea
arahansa <arahansa@naver.com>
2017-02-08 09:27:16 +0900

    add cross entropy_err

--- ml/04_신경망학습/02_손실함수/01_평균제곱오차.py
+++ ml/04_신경망학습/02_손실함수/01_평균제곱오차.py
@@ -16,6 +16,7 @@ print(mean_squared_error(np.array(y),np.array(t)))
 
 
 def cross_entropy_error(y,t):
+    print("t :", t, "y : ", y)
     delta = 1e-7
     return -np.sum(t * np.log(y + delta))
 
@@ -28,3 +29,31 @@ print(cross_entropy_error(np.array(y), np.array(t)))
 y = [0.1, 0.05, 0.1, 0.0, 0.05, 0.1, 0.0, 0.6, 0.0, 0.0]
 print(cross_entropy_error(np.array(y), np.array(t)))
 
+
+def cross_entropy_error(y,t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+
+    print("t :", t , "y : ", y)
+    batch_size = y.shape[0]
+    delta = 1e-7
+    print("batch size :", batch_size)
+    return -np.sum( t * np.log(y+ delta)) / batch_size
+
+print("---")
+y = [0.1, 0.05, 0.1, 0.0, 0.05, 0.1, 0.0, 0.6, 0.0, 0.0]
+print(cross_entropy_error(np.array(y), np.array(t)))
+
+y = [[0.1, 0.05, 0.1, 0.0, 0.05, 0.1, 0.0, 0.6, 0.0, 0.0], [0.1, 0.05, 0.3, 0.3, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0]]
+t = [[0, 0, 1, 0, 0, 0, 0, 0, 0, 0], [0,0,1,0,0,0,0,0,0,0]]
+print(cross_entropy_error(np.array(y), np.array(t)))
+
+# 정답 레이블이 원핫인코딩이 ㄴ아니라 2, 7 등의 숫자 레이블로 주어졌을 때의 경우
+# def cross_entropy_error(y,t):
+#     if y.dim == 1:
+#         t = t.reshape(1, t.size)
+#         y = y.reshape(1, y.size)
+#
+#     batch_size = y.shape[0]
+#     return -np.sum( np.log(y[np.arange(batch_size), t])) / batch_size
